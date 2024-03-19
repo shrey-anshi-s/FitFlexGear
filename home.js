b98a1c320429eb844d746de83ba839f0780258d8
shrey-anshi-s <120453595+shrey-anshi-s@users.noreply.github.com>
2024-03-19 15:33:11 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    overlay.style.display = "block";
+  }
+  
+
+
